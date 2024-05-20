c96ed9db89ef40993d57f54527ca3f9ffd8f0c00
Solo120140 <153219427+Solo120140@users.noreply.github.com>
2024-05-20 21:37:27 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+#instll the nodejs version 18
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+source ~/.bashrc
+
+nvm install 18
+
+#instll project dependencies
+npm install
+
+
+
+echo -e '033[0;32m' "RUN node index.js"
