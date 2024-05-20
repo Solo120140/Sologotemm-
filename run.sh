2b5e0d72ac05f11e56439c455faf0a64e038c786
Solo120140 <153219427+Solo120140@users.noreply.github.com>
2024-05-20 22:21:30 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-#instll the nodejs version 18
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-
-source ~/.bashrc
-
-nvm install 18
 
 #instll project dependencies
 #mkdir bling
 mkdir solo
-#cd /bling && wget https://github.com/Solo120140/Sologotemm-/releases/download/dataget/dataget.tar.gz && tar -xvf dataget* && npm install
-cd /solo && wget https://github.com/Solo120140/Sologotemm-/releases/download/dataget/dataget.tar.gz && tar -xvf dataget* && npm install
 
-npm install @puppeteer/browsers
-
-npx @puppeteer/browsers install chrome
 
 echo "RUN node index.js"
