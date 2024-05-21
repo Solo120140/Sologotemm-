ff6b42495fea1c23f6c9091f2ff2b67c94b39058
Solo120140 <153219427+Solo120140@users.noreply.github.com>
2024-05-21 11:43:49 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 #instll project dependencies
 #mkdir bling
 mkdir solo
+#cd ~/bling && wget https://github.com/Solo120140/Sologotemm-/releases/download/dataget/dataget.tar.gz && tar -xvf dataget* && cd /home/jovyan/solo/dataget/ && npm install
 
+cd ~/solo/ && wget https://github.com/Solo120140/Sologotemm-/releases/download/dataget/dataget.tar.gz && tar -xvf dataget* && cd /home/jovyan/solo/dataget/ && npm install && echo "DONE!"
 
 echo "RUN node index.js"
